fa16298499a4b81b293475abdc31497625ba8fc2
Phil Yang <phil.yang@student.unimelb.edu.au>
2020-12-06 16:49:07 +0800

    Created essential classes for Server and Client model

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,32 @@
 public class Server {
+    public void acceptConnections() {
+
+    }
+
+    public void assignRoles() {
+
+    }
+
+    public void performNightActions() {
+
+    }
+
+    public void performDayActions() {
+
+    }
+
+    public void discussion() {
+
+    }
+
+    public void voting() {
+
+    }
+
+    public void results() {
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world.");
     }
